b643a65e388f2220585f89e97e7385a81879c744
Karthick0529 <karthicknandha619@gmail.com>
2024-10-03 11:19:56 +0530

    updated

--- utils/http.js
+++ utils/http.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL:`https://sparklepro-be.onrender.com/api`
-    baseURL:"http://localhost:3000/api"
+    baseURL:`https://sparklepro-be.onrender.com/api`
+    // baseURL:"http://localhost:3000/api"
     // timeout:1000
 })
 
